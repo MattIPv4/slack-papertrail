ba114cf5297549b40b1e0782584db31e0de21b8e
MattIPv4 <me@mattcowley.co.uk>
2025-02-18 18:55:04 +0000

    Add full messages for emoji changes

--- src/index.ts
+++ src/index.ts
@@ -30,9 +30,44 @@ app.event("emoji_changed", async ({ event }) => {
   console.log("Received emoji_changed event", event);
   if (!channelEmoji) return;
 
+  // Slack exposes both a name and names, and seems to use both in different contexts
+  const names = Array.from(
+    new Set([event.name, ...(event.names || [])]),
+  ).filter(Boolean);
+
+  // Aliases
+  if (event.subtype === "add" && event.value?.startsWith("alias:")) {
+    const alias = event.value.slice(6);
+    await app.client.chat.postMessage({
+      channel: channelEmoji,
+      text: names
+        .map(
+          (name) =>
+            `:linked_paperclips: :${name}: \`:${name}:\` \`:${alias}:\``,
+        )
+        .join("\n"),
+      mrkdwn: true,
+    });
+    return;
+  }
+
+  // Removals
+  if (event.subtype === "remove") {
+    await app.client.chat.postMessage({
+      channel: channelEmoji,
+      text: names.map((name) => `:heavy_minus_sign: \`:${name}:\``).join("\n"),
+      mrkdwn: true,
+    });
+    return;
+  }
+
+  // Additions
   await app.client.chat.postMessage({
     channel: channelEmoji,
-    text: `Emoji ${event.subtype}: ${event.name || event.names?.join(", ")}`,
+    text: names
+      .map((name) => `:heavy_plus_sign: :${name}: \`:${name}:\``)
+      .join("\n"),
+    mrkdwn: true,
   });
 });
 
